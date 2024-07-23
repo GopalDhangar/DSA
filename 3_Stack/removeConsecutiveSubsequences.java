b512e34e557e2b823ed601bcc1014ac27cfb6884
Gopal_Dhangar <144979013+GopalDhangar@users.noreply.github.com>
2024-07-23 13:26:55 +0530

    Update removeConsecutiveSubsequences.java

--- 3_Stack/removeConsecutiveSubsequences.java
+++ 3_Stack/removeConsecutiveSubsequences.java
@@ -20,6 +20,8 @@ public class removeConsecutiveSubsequences {
         return res;
     }
 
+// OUTPUT {1,3,5,1}
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 2, 3, 10, 10, 10, 4, 4, 4, 5, 7, 7, 1 };
         int[] res = remove(arr);
